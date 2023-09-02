6c93c3ddc2311beefb981516aeacffa2678180ed
AKHATAR Oussama <116565849+oussama-akhatar@users.noreply.github.com>
2023-09-02 13:29:01 +0100

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This is a simple shell script to calculate simple interest
+
+# Input values
+read -p "Enter the principal amount: " principal
+read -p "Enter the annual interest rate (as a decimal): " rate
+read -p "Enter the time (in years): " time
+
+# Calculate simple interest
+interest=$(echo "$principal * $rate * $time" | bc)
+
+# Display the result
+echo "Simple Interest: $interest"
